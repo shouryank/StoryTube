37261a8105ba77bc007886085149e60128008d41
shouryank <shouryank01@gmail.com>
2022-11-09 16:54:51 +0530

    refactor: Added new features to main.py and animat

--- animation/animate.py
+++ animation/animate.py
@@ -21,133 +21,152 @@ dir_list = {'r' : 200, 'l' : 400} #moving towards which direction
 
 dialogues = []
 dialogue_count = 0
- 
+
+# Get actions_movement
+actions_movement = {'die' : 0, 'fall' : 0, 'hurt' : 0, 'idle' : 0, 'jump' : 1, 'run' : 1, 'slide' : 1, 'walk' : 1, 'say': 0}
+
 class MySprite(pygame.sprite.Sprite):
 
-    def __init__(self, char, x, y, dir, svos, actions_movement):
+    def __init__(self, char, x, y, dir):
         super(MySprite, self).__init__()
         
         self.images = {}
         self.actions = []
         # self.images = [pygame.image.load(img) for img in glob.glob("pygame\\cat\\*.png")]
-        for action in svos[char]:
-            if type(action) == list:
-                dialogues.append(action[1])
-
-            # self.actions.append(action[0] if type(action) == list else action)
-                        
-            action = WordNetLemmatizer().lemmatize(action[0] if type(action) == list else action,'v')   
-            self.actions.append(action)
-            print(self.actions)
-
-            if action not in self.images:
-                self.images[action] = [pygame.transform.scale(pygame.image.load(img) , (200,200)) for img in glob.glob(character_path + char + "\\" + action + "\\*")]
+        # for action in svos[char]:
+        #     if type(action) == list:
+        #         dialogues.append(action[1])
+        #     action = WordNetLemmatizer().lemmatize(action[0] if type(action) == list else action,'v')   
+        #     self.actions.append(action)
+        #     print(self.actions)
             
-        print(self.images)
+        #     if action not in self.images:
+        #         self.images[action] = [pygame.transform.scale(pygame.image.load(img) , (200,200)) for img in glob.glob("assets\\characters" + char + "\\" + action + "\\*.png")]
+            
+        # print(self.images)
         self.char = char
         self.index = 0
         self.action_count = 0
         self.dir = dir
         self.x = x
         self.y = y
-        self.svos = svos
-        self.actions_movement = actions_movement
-        self.image = None
+        self.prev_action = None  
 
-    def play_dialogue(self, index):
-        myobj = gTTS(text=dialogues[index], lang=language, slow=False)
+    def play_dialogue(self, dialogue):
+        myobj = gTTS(text=dialogue, lang=language, slow=False)
         myobj.save("dialogue.mp3")
         with audioread.audio_open('dialogue.mp3') as f:
             totalsec = f.duration
+
         pygame.mixer.init()
         pygame.mixer.music.load("dialogue.mp3")
         pygame.mixer.music.set_volume(1)
         pygame.mixer.music.play()
         sleep(totalsec)
         
-    def movement_update(self):
+        pygame.mixer.music.unload()
+
+        
+    def movement_update(self, fps = FPS):
         # if self.x == 880:
         #     self.dir = 'l'
         # elif self.x == 0:
         #     self.dir = 'r'
-
         if self.dir == 'r':                        
-            self.x += FPS
+            self.x += fps
         else:
             self.image = pygame.transform.flip(self.image, True, False)
-            self.x -= FPS
+            self.x -= fps
         screen.blit(self.image, (self.x, self.y))
  
-    def update(self):
-        # print(self.index, self.action_count, len(self.actions), len(self.images[self.actions[self.action_count]]))
-        print(self.actions[self.action_count])
-        if self.actions[self.action_count] == "say":
-            global dialogue_count
-            self.play_dialogue(dialogue_count)
-            dialogue_count += 1
-            print("dialogue played")
-        if self.index >= len(self.images[self.actions[self.action_count]]):
-            if self.action_count >= len(self.actions) - 1:
-                return 1
+    def update(self, action, dialogue):
+        # Get images that are used to animate the given action
+        flag = 0
+        if action not in self.images or len(self.images[action]) == 0:
+            print("Adding images")
+            self.images[action] = [pygame.transform.scale(pygame.image.load(img) , (200,200)) for img in glob.glob("assets\\characters\\" + self.char + "\\" + action + "\\*.png")]
+            print("Self.images = ", self.images)
+
+        if self.index >= len(self.images[action]):
+            flag = 1
             self.index = 0
-            self.action_count += 1
-        self.image = self.images[self.actions[self.action_count]][self.index]
-        self.index += 1
+            self.prev_action = action
 
-        if self.actions_movement[self.actions[self.action_count]]:
-            self.movement_update()
+        if not flag:
+            self.image = self.images[action][self.index]
+            self.index += 1
+
+            self.movement_update(FPS if actions_movement[action] else 0)
+
+            # # Say dialogue
+            # if action == "say":
+            #     self.play_dialogue(dialogue)
+            #     print("dialogue played")
         else:
-            screen.blit(self.image, (self.x, self.y))
+            self.prev_action = action
+            self.play_prev_frame()
         
-        return 0
 
-    def update_idle(self):
-        self.image = pygame.transform.scale(pygame.image.load(character_path + self.char + "/idle/idle1.png") , (200,200))
-        flip_var = True if self.dir == "l" else False
-        print(flip_var)
-        self.image = pygame.transform.flip(self.image, flip_var, False)
-        screen.blit(self.image, (self.x, self.y))
+        print("self.index: ", self.index - 1)
+
+        return flag
+
+    def play_prev_frame(self):
+        if self.prev_action is None:
+            return
+        
+        self.image = self.images[self.prev_action][-1]
+
+        print("PLAYING PREVIOUS ACTION ", self.prev_action, " OF ", self.char, " IMAGE ", self.image)
+
+        self.movement_update(0)
         
  
-def animate(characters, svos, actions_movement, extracted_weather):
+def animate(characters, svos, extracted_weather):
     bg = pygame.transform.scale(pygame.image.load(weather_path + extracted_weather + '.jpg') , SIZE)
 
     pygame.init()
     pygame.display.set_caption("Trace")
-    char_objects = []
-    for i in range(len(characters)):
-        dir = list(dir_list.keys())[i % 2]
-        char_objects.append(MySprite(characters[i], dir_list[dir] * ((i % 2) + 1), 480, dir, svos, actions_movement))
-    # my_group = pygame.sprite.Group(my_sprite)
 
-    idle_char = []
+    char_objects = dict()
+
+    for i, character in enumerate(characters):
+        dir = list(dir_list.keys())[i % 2]
+        char_objects[character] = MySprite(character, dir_list[dir] * ((i % 2) + 1), 480, dir)
+
+    # my_group = pygame.sprite.Group(my_sprite)
     clock = pygame.time.Clock()
-    
-    loop = 1
-    count = 0 # number of characters whos actions have been completed
 
 
     return_val = 0
-    while count < len(characters): 
+    for svo in svos:
+        print(svo)
+        character = svo[0]
+        action = svo[1]
+        dialogue = ""
 
-        pygame.event.get() 
+        if(len(svo) == 3):
+            dialogue = svo[2]
         
-        screen.fill((0,0,0))        
-        screen.blit(bg, (0, 0))
+        flag = 0
+        pygame.event.get()
 
-        for char in char_objects:
-            return_val = char.update()   
-            print(return_val) 
-            if return_val == 1:
-                char_objects.remove(char)
-                idle_char.append(char)
-                count += return_val
-        
-        for char in idle_char:
-            print(char.x)
-            char.update_idle()
+        while not flag:
+            screen.fill((0,0,0))        
+            screen.blit(bg, (0, 0))
 
-        pygame.display.update()
-        clock.tick(FPS)
+            flag = char_objects[character].update(action, dialogue)
+
+            for char in char_objects:
+                if char != character:
+                    char_objects[char].play_prev_frame()
+
+            pygame.display.update()
+            clock.tick(FPS)
+
+            if dialogue != "":
+                char_objects[character].play_dialogue(dialogue)
+                print("dialogue played")
+                dialogue = ""
 
     pygame.quit()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import py_compile
 from coref_resolution import coref
-from clausIE import char_to_action, svo, weather_extraction
+from clausIE import refactor_svo, svo, weather_extraction
 from nltk.stem.snowball import SnowballStemmer
 import nltk
 from animation import animate
 import gui
 
-text = gui.txt1.get(1.0, "end-1c")
+text = gui.text
 print(text)
 
 # text = """A cat is walking on a snowy day. It jumped over a stone. It died. It ran. Dog is walking in the opposite direction. It ran. The dog said "Hello world, the cat is going to die hahaha".""" #  Dog is walking in the opposite direction. It ran. The dog said "Hello world"."""
@@ -21,19 +21,14 @@ weather = weather_extraction.get_weather(text)
 svos = svo.extract_svo(text, corefed_text)
 
 # Assign char to action
-svos = char_to_action.assign_char_to_action(svos)
+svos, characters = refactor_svo.refactor_svo(svos)
 
 # Create new stemmer for the words
 stemmer = SnowballStemmer("english")
 
-# Get actions_movement
-actions_movement = {'die' : 0, 'fall' : 0, 'hurt' : 0, 'idle' : 0, 'jump' : 1, 'run' : 1, 'slide' : 1, 'walk' : 1, 'say': 0}
 #word2vec similarity between the incoming action vs the ones we have in list of actions and then set a threshold and execute the action based on it
 
 # nltk.download('omw-1.4')
 # nltk.download('wordnet')
 
-characters = char_to_action.get_characters(svos)
-print(characters)
-
-animate.animate(characters, svos, actions_movement, weather)
+animate.animate(characters, svos, weather)
