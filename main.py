1b67ecf20938650e40f08e9df1d2c9676b264e69
shouryank <shouryank01@gmail.com>
2022-11-09 11:09:26 +0530

    feat: added gui to main

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from clausIE import char_to_action, svo, weather_extraction
 from nltk.stem.snowball import SnowballStemmer
 import nltk
 from animation import animate
+import gui
 
-text = """A cat is walking on a snowy day. It jumped over a stone. It died. It ran. Dog is walking in the opposite direction. It ran. The dog said "Hello world, the cat is going to die hahaha".""" #  Dog is walking in the opposite direction. It ran. The dog said "Hello world"."""
+text = gui.txt1.get(1.0, "end-1c")
+print(text)
+
+# text = """A cat is walking on a snowy day. It jumped over a stone. It died. It ran. Dog is walking in the opposite direction. It ran. The dog said "Hello world, the cat is going to die hahaha".""" #  Dog is walking in the opposite direction. It ran. The dog said "Hello world"."""
 
 # Coref resolution
 corefed_text = coref.resolve_coref(text)
